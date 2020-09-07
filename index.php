0c095b0c49ec4c61e9828b711ea4c4ee0595ff26
Praneel Mahalatkar <pmahalatkar@gmail.com>
2020-09-06 19:40:29 -0500

    Update Alpha 0.006

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
       <pre class="prfrmt">                               <button onclick="window.location.href='index.php';" class="button button1" type="button" name="button">Home</button></pre>
       <pre class="prfrmt">                               <button onclick="window.location.href='#';" class="button button1" type="button" name="button">Link1</button></pre>
       <pre class="prfrmt">                               <button onclick="window.location.href='#';" class="button button1" type="button" name="button">Link2</button></pre>
-      <pre class="prfrmt">                               <button onclick="window.location.href='chatpage.php';" class="button button1" type="button" name="button">Chat</button></pre>
+      <pre class="prfrmt">                               <button onclick="window.location.href='#';" class="button button1" type="button" name="button">Link3</button></pre>
     </nav>
     <div class="header">
         <br><br>
@@ -35,5 +35,6 @@
           <h2 class="maintitle">Welcome to the Minecraft Website!</h2>
       </div>
     </div>
+    <div id="rt-9414fa4b1ff911329edb01cd5df7b608" data-floating="true" data-side="right" data-width="700" data-height="500" data-counter="14:23"></div> <script src="https://rumbletalk.com/client/?rB@WydY:"></script>
   </body>
 </html>
